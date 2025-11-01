634c345513a336beb126a2e7fd0cef93bf8e452e
alycano <aly.cano@udea.edu.co>
2025-10-31 19:24:06 -0500

    feat: Trabajar con cadenas

---
+++ src/main/java/Cadenas.java
@@ -0,0 +1,6 @@
+public class Cadenas {
+    public static void main(String[] args) {
+        String nombre = "Aly Santiago";
+        System.out.println("Hola, " + nombre + "! Bienvenido al laboratorio de Java.");
+    }
+}
