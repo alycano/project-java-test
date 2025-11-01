3ec5c2ccc81bf4634c2dbb6847ff65169939d252
alycano <aly.cano@udea.edu.co>
2025-10-31 19:21:33 -0500

    feat: Trabajar con números de punto flotante

---
+++ src/main/java/NumerosFlotantes.java
@@ -0,0 +1,7 @@
+public class NumerosFlotantes {
+    public static void main(String[] args) {
+        double temperatura = 36.7;
+        double conversion = temperatura * 9 / 5 + 32;
+        System.out.println("Temperatura en Fahrenheit: " + conversion);
+    }
+}
